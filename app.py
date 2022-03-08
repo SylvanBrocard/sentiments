e9aa1a5306d3fb9e5b7b20e43ac4f0a807304a17
Rubal Ravinder <rubal.ravinder@gmail.com>
2022-03-08 16:59:08 +0100

    connect to db

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from locale import currency
 from flask import Flask, render_template
 from forms import ContactForm
 from flask import request
 import pandas as pd
+import psycopg2
 
 ###############################################
 #          Define flask app                   #
@@ -24,13 +26,26 @@ def get_contact():
     # here, if the request type is a POST we get the data on contat
     #forms and save them else we return the contact forms html page
     if request.method == 'POST':
-        name =  request.form["name"]
-        email = request.form["email"]
-        subject = request.form["subject"]
-        message = request.form["message"]
-        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
-        res.to_csv('./contactusMessage.csv')
+        name =  "'" + request.form["name"] + "'"
+        email = "'" + request.form["email"] + "'"
+        subject = "'" + request.form["subject"] + "'"
+        message = "'" + request.form["message"] + "'"
+        global id
+        print(f"""INSERT INTO clients (id, name, email, subject, message) VALUES ({id},{name},{email},{subject},{message});""")
+        
+        conn = get_connection()
+        conn.autocommit = True
+
+        curr = conn.cursor()
+
+        curr.execute(f"""INSERT INTO clients (id, name, email, subject, message) VALUES ({id},{name}, {email},{subject},{message});""")
+        id+=1
+
+        conn.close()
+        # res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        # res.to_csv('./contactusMessage.csv')
         return "<p>Merci de nous avoir contactés.</p>"
+
     else:
         return render_template('contact.html', form=form)
 
@@ -38,5 +53,43 @@ def get_contact():
 def about():
     return render_template('about.html')
 
+
+
+###############################################
+#       Connection to postgres database       #
+###############################################
+def get_connection():
+    try:
+        return psycopg2.connect(
+            database="messages",
+            user="wym_admin",
+            password="admin",
+            host="localhost",
+            port=5432,
+        )
+    except:
+        return False
+
+
+conn = get_connection() # create connection object
+
+conn.autocommit = True
+if conn:
+    print("Connection to the PostgreSQL established successfully.")
+else:
+    print("Connection to the PostgreSQL encountered an error.")
+
+curr = conn.cursor() # cursor object
+
+# create table in database
+curr.execute("""CREATE TABLE IF NOT EXISTS clients (
+                id serial PRIMARY KEY,
+                name TEXT,
+                email TEXT,
+                subject TEXT,
+                message TEXT);
+                """)
+id = 0 # initialize id at 0
+conn.close()
 
 app.run(debug=True)
