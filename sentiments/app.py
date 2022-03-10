e03334bc9d7ed3801b1c72119269449e4874769e
SylvanBrocard <sylvan.brocard@gmail.com>
2022-03-10 11:27:56 +0100

    black formatting

--- sentiments/app.py
+++ sentiments/app.py
@@ -17,12 +17,13 @@ DB_PORT = os.getenv("DB_PORT")
 #          Define flask app                   #
 ###############################################
 app = Flask(__name__)
-app.secret_key = 'secretKey'
+app.secret_key = "secretKey"
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
 ###############################################
@@ -32,34 +33,34 @@ def home():
 def get_contact():
     form = ContactForm()
     # here, if the request type is a POST we get the data on contat
-    #forms and save them else we return the contact forms html page
-    if request.method == 'POST':
-        name =  request.form["name"]
+    # forms and save them else we return the contact forms html page
+    if request.method == "POST":
+        name = request.form["name"]
         email = request.form["email"]
         subject = request.form["subject"]
         message = request.form["message"]
         global id
-        
+
         conn = get_connection()
 
         curr = conn.cursor()
 
         curr.execute(
             """INSERT INTO clients (id, name, email, subject, message) VALUES (%s, %s, %s, %s, %s);""",
-            (id, name, email, subject, message)
+            (id, name, email, subject, message),
         )
-        id+=1
+        id += 1
 
         conn.close()
         return "<p>Merci de nous avoir contactés.</p>"
 
     else:
-        return render_template('contact.html', form=form)
+        return render_template("contact.html", form=form)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
-
+    return render_template("about.html")
 
 
 ###############################################
@@ -79,28 +80,31 @@ def get_connection():
     except:
         return False
 
+
 conn = False
 while not conn:
-    conn = get_connection() # create connection object
+    conn = get_connection()  # create connection object
 
     if conn:
         print("Connection to the PostgreSQL established successfully.", flush=True)
-        curr = conn.cursor() # cursor object
+        curr = conn.cursor()  # cursor object
         # create table in database
-        curr.execute("""CREATE TABLE IF NOT EXISTS clients (
+        curr.execute(
+            """CREATE TABLE IF NOT EXISTS clients (
                         id serial PRIMARY KEY,
                         name TEXT,
                         email TEXT,
                         subject TEXT,
                         message TEXT);
-                        """)
-        
+                        """
+        )
+
         # Get max id
         curr.execute("""SELECT MAX(id) from clients""")
         id = curr.fetchone()[0]
-        id = 0 if id is None else id+1
+        id = 0 if id is None else id + 1
 
-        conn.close() # close connection to database
+        conn.close()  # close connection to database
     else:
         print("Waiting for connection to PostgreSQL...", flush=True)
         sleep(2)
@@ -113,16 +117,12 @@ while not conn:
 def summarize():
     form = SummarizeForm()
     # here, if the request type is a POST we get the data from
-    #forms and save them else we return the forms html page
-    if request.method == 'POST':
-        userTxt =  '"""' + escape(request.form["text"]) + '"""'
+    # forms and save them else we return the forms html page
+    if request.method == "POST":
+        userTxt = '"""' + escape(request.form["text"]) + '"""'
 
         url = "http://model:5000/model/predict"
-        data = {
-        "text": [
-            userTxt
-            ]
-        }
+        data = {"text": [userTxt]}
 
         res = requests.post(url, json=data)
         test = json.loads(res.text)
@@ -130,7 +130,7 @@ def summarize():
         return f"""<h2>Your text</h2> <p> {userTxt} </p> <h2>Your text summarized </h2> <p>{test['summary_text'][0]}</p>"""
 
     else:
-        return render_template('model.html', form=form)
+        return render_template("model.html", form=form)
 
 
 app.run(host="0.0.0.0", port=8000, debug=True)
--- sentiments/forms.py
+++ sentiments/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
+
+
 class ContactForm(FlaskForm):
     name = StringField("Name")
     email = StringField("Email")
     subject = StringField("Subject")
     message = TextAreaField("Message")
     submit = SubmitField("Send")
+
 
 class SummarizeForm(FlaskForm):
     text = TextAreaField("Text")
